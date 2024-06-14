c6a9c3a53bc0ec7d1107f5de287dafc4e4a33688
Danny Bierek <dannybierek@gmail.com>
2024-06-14 11:18:48 -0700

    Changed params to in_file and out_file for connect_to_game

--- examples/connect_to_game.rs
+++ examples/connect_to_game.rs
@@ -1,4 +1,6 @@
 use std::{env, fs};
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
 use tungstenite::{connect, Message};
 use url::Url;
@@ -6,23 +8,36 @@ use url::Url;
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: cargo run -- example connect_to_game <port>");
+        eprintln!("Usage: cargo run -- example connect_to_game <in_file> <out_file>");
         return;
     }
-    println!("Connecting to game on port {}", args[1]);
+    println!("Connecting to game on port 46735");
 
     let (mut socket, _response) = connect(
-        Url::parse(("ws://localhost:".to_string() + &args[1] + "/socket").as_str()).unwrap()
+        Url::parse("ws://localhost:46735/socket").unwrap()
     ).expect("Can't connect");
     
     let _ = socket.write_message(Message::Text(r#"{"type":"hello","identifier":"glacier2obj"}"#.into()));
-    // let message = r#"{"type":"listTbluEntities", "tblus":["002E141E1B1C6EFE","006987D492265FBC","00C7E348A80A6E6E","006734FBA4F1E68D","0055D313795CFA48","008A2B97E3510758","00B6E9D9DA6DE143","00F032EB6C229123"]}"#;
-    let message = fs::read_to_string("toFind.json").expect("Error opening toFind.json. Run a scan to generate this file");
+    let message = fs::read_to_string(args[1].as_str()).expect(format!("Error opening {}. Run a scan to generate this file", args[1].as_str()).as_str());
 
     let _ = socket.write_message(Message::Text(message.into()));
     
+    fs::write(args[2].as_str(), "").expect(format!("Error writing to {}", args[2].as_str()).as_str());
+    
+    // let mut entities = "[".to_string();
+    let mut out_file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(args[2].as_str())
+        .unwrap();
+
     loop {
         let msg = socket.read_message().expect("Error reading message");
         println!("Received: {}", msg);
+        // entities += msg.to_string().as_str();
+        if let Err(e) = writeln!(out_file, "{}", msg) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
     }
+    // entities += "]";
 }
